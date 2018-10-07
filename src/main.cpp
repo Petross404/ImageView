ff1bd4891a626da732958be70fda6a5ce2d07597
Petros <petross404@gmail.com>
2018-10-07 12:35:51 +0300

    Initial import

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "imageview.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    ImageView w;
+    w.show();
+
+    return app.exec();
+}
+
